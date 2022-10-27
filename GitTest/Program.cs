655b9ccc4929ce74cdce8ed496f2d2f271b00494
DESKTOP-1PO3I2A\tele8 <tele81427>
2022-10-27 18:00:21 +0800

    first version

---
+++ GitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test1");
+        }
+    }
+}
